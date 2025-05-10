f21cad9fd99b1803f3e99fd772766351a3275804
Jhon Alex <fern@gmail.com>
2025-05-10 08:31:15 -0500

    Mejoras en la ejecución

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:ordenado/pages/login.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({super.key});
 
+  final valorEnv = const String.fromEnvironment('API_KEY');
+
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text('Manejo del minimercado'),
-      ),
-      body: const Center(
-        child: Text('Empezar con el manejo de los sistemas'),
+    return PopScope(
+      canPop: true,
+      onPopInvokedWithResult: (didPop, result) => Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => Login(),)),
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('Manejo del minimercado'),
+        ),
+        body: Center(
+          child: Text('Valor variable de entorno: $valorEnv'),
+        ),
       ),
     );
   }
